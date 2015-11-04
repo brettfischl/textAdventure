0189fe017588cc836543d0ea489d78cba2437470
Alpere Fischl <aubnox@gmail.com>
2015-11-03 21:09:05 -0500

    build script for frontend

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var gutil = require('gulp-util');
+
+var del = require('del');
+
+var chalk = require('chalk');
+
+
+var DEST_PATH = './dist/';
+
+gulp.task('build', function () {
+  return gulp.src('./src/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(DEST_PATH))
+    .pipe(rename({ extname: '.min.js'}))
+    // .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+    //     .on('error', gutil.log)
+    .pipe(sourcemaps.write(DEST_PATH))
+    .pipe(gulp.dest(DEST_PATH));
+});
+
+gulp.task('clean', function() {
+
+});
